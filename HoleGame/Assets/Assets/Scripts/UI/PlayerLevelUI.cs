89907302e6f5037ea75e34dedf5c651df1964982
Miroslav Miroslavov <martinsen06@gmail.com>
2025-12-18 18:33:09 +0200

    1.11.5 Implement show level text with level up

--- HoleGame/Assets/Assets/Scripts/UI/PlayerLevelUI.cs
+++ HoleGame/Assets/Assets/Scripts/UI/PlayerLevelUI.cs
@@ -8,9 +8,16 @@ using DG.Tweening;
 public class PlayerLevelUI : MonoBehaviour
 {
   public TMP_Text levelText;
+  public TMP_Text hideLevelText;
   public Vector3 scaleParams = new Vector3(2f,2f,2f);
   public float scaleDuration = 0.1f;
+  public float moveHideTextDistance = 5f;
+  public float moveHideTextDuration = 0.3f;
   public ParticleSystem rippleVFX;
+
+  private void Awake() {
+    hideLevelText.fontSize = 0;
+  }
 
 
 
@@ -28,5 +35,24 @@ public class PlayerLevelUI : MonoBehaviour
   {
     rippleVFX.Play();
   }
-  
+  public void ShowAndHideLevelText(int level)
+  {
+    // 1. Початкові налаштування
+    hideLevelText.text = "Level " + level.ToString();
+    hideLevelText.fontSize = 30;
+
+    // Запам'ятовуємо початкову локальну позицію, щоб точно повернутися назад
+    Vector3 startPos = hideLevelText.transform.localPosition;
+
+    // 2. Анімація польоту вгору по осі Y
+    hideLevelText.transform.DOLocalMoveY(startPos.y + moveHideTextDistance, moveHideTextDuration)
+        .SetEase(Ease.OutQuad) // Плавне сповільнення в кінці
+        .OnComplete(() =>
+        {
+          // 3. Скидання після завершення
+          hideLevelText.fontSize = 0;
+          hideLevelText.transform.localPosition = startPos; // Повертаємо в точну початкову точку
+        });
+  }
+
 }
