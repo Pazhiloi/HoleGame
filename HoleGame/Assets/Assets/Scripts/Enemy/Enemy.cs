5e3949730e9b1f57d9b25f17011a6407265b2c4a
Miroslav Miroslavov <martinsen06@gmail.com>
2026-04-07 18:01:22 +0300

    1.15.1 Start implementing Enemy UI

--- HoleGame/Assets/Assets/Scripts/Enemy/Enemy.cs
+++ HoleGame/Assets/Assets/Scripts/Enemy/Enemy.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-  public SphereCollider enemyCollider;
   public bool isConsumed = false;
+  public bool isReqForVictory = false;
   public int expforEnemy = 1;
 
-  private void Awake() {
-    enemyCollider = GetComponent<SphereCollider>();
-  }
 }
