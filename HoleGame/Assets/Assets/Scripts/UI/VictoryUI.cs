8ce0eee2041ff9cbb1d7711652cf8613cdd47dcb
Miroslav Miroslavov <martinsen06@gmail.com>
2026-04-29 05:58:24 +0300

    1.16.1 Add burst vfx to victory Panel

--- HoleGame/Assets/Assets/Scripts/UI/VictoryUI.cs
+++ HoleGame/Assets/Assets/Scripts/UI/VictoryUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class VictoryUI : MonoBehaviour
@@ -7,6 +8,8 @@ public class VictoryUI : MonoBehaviour
   [SerializeField] private GameObject victoryPanel;
   [SerializeField] private CanvasGroup canvasGroup; // Додай CanvasGroup на VictoryPanel для плавного проявлення
   [SerializeField] private RectTransform victoryText;
+  [Header("Ефекти салюту")]
+  [SerializeField] private List<ParticleSystem> fireworks;
 
   private void Awake()
   {
@@ -26,7 +29,23 @@ public class VictoryUI : MonoBehaviour
     if (victoryText != null)
     {
       victoryText.localScale = Vector3.zero;
-      victoryText.DOScale(Vector3.one, 0.8f).SetEase(Ease.OutBack);
+      victoryText.DOScale(Vector3.one, 0.8f).SetEase(Ease.OutBack).OnComplete(() => 
+                {
+                    // Цей код виконається ТІЛЬКИ після завершення анімації тексту
+                    LaunchFireworks();
+                });;
     }
   }
+
+  private void LaunchFireworks()
+  {
+    float delay = 0f;
+    foreach (ParticleSystem ps in fireworks)
+    {
+      // Запускаємо через невелику затримку (наприклад, кожні 0.2 сек)
+      DOVirtual.DelayedCall(delay, () => ps.Play());
+      delay += 0.2f;
+    }
+  }
+
 }
